7ed3ea672e9cfea80d994e2be5cee303d665b5c3
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2022-10-30 23:34:34 -0300

    Implementando DTO

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.modelo.Topico;
+
+public class TopicoDto {
+	
+	//Aqui nessa classe eu defino somente os campos que eu quero que a minha API devolva
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+	
+	//Construtor
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	//Transformar Topico (model) para TopicoDto
+	public static List<TopicoDto> converter(List<Topico> topicos) {
+		/**
+		 * Pega a lista de Topicos, faz um "for", pra cada Topico, dar um "new" no TopicoDto,
+		 * guardar numa lista de TopicoDto e devolver essa lista de TopicoDto
+		 **/
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	}
+	
+}
